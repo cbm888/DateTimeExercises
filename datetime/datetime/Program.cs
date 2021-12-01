e9b1054c2b084965393551c04d6aeae5b5229b15
cbm888 <cbmarch88@gmail.com>
2021-12-01 03:43:34 -0600

    Add files via upload

---
+++ datetime/datetime/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace datetime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var today = new DateTime();
+
+            today = DateTime.Now;
+
+            var yesterday = today.AddDays(-1);
+            Console.WriteLine(yesterday);
+
+
+            Console.WriteLine(today.ToLongDateString());
+            Console.WriteLine(today.ToShortDateString());
+
+            Console.WriteLine(today.ToLongTimeString());
+            Console.WriteLine(today.ToShortTimeString());
+        }
+
+        public void dateTime()
+        {
+            var timeSpan = new TimeSpan(1, 2, 3);
+            
+
+            TimeSpan.FromHours(1);
+            var start = DateTime.Now;
+            var end = DateTime.Now.AddMinutes(2);
+            var duration = end - start;
+        }
+
+    }
+}
